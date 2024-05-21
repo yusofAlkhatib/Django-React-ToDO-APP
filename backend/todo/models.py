b4f06ea31e1d4bededd57d06ea23ad88de5276c8
yusofalkhatib <yusof94@hotmail.com>
2024-05-21 19:13:56 +0200

    create django project & design todo app models

---
+++ backend/todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.CharField(choices=[('DONE','DONE'),('INPROGRERSS','INPROGRERSS')])
+    created_at = models.DateField(defult=timezone.now)
+
+    def __str__(self):
+        return self.title
+
