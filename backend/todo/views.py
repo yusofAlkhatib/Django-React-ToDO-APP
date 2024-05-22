718ca3be2a64095bc198862f5af04232dfae7718
yusofalkhatib <yusof94@hotmail.com>
2024-05-22 19:57:47 +0200

    add API using Viewsets

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ToDoSerializer
+from .models import ToDo
 
 # Create your views here.
+class ToDoViewSet(viewsets.ModelViewSet):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
